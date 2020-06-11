4a2150db9846b37f5c1f6abdf1933aa3dc52c54c
Rúben André Barreiro <ruben.andre.letra.barreiro@gmail.com>
2020-06-11 12:36:38 +0100

    Some Coordinates' Systems added

---
+++ src/main/java/org/quist/qubits/geometry/utils/coordinates/Coordinate3D.java
@@ -0,0 +1,79 @@
+package org.quist.qubits.geometry.utils.coordinates;
+
+import java.util.Objects;
+
+public abstract class Coordinate3D {
+
+    private Object coordinateNo1;
+    private Object coordinateNo2;
+    private Object coordinateNo3;
+
+    public Coordinate3D(Object coordinateNo1, Object coordinateNo2, Object coordinateNo3) {
+
+        this.coordinateNo1 = coordinateNo1;
+        this.coordinateNo2 = coordinateNo2;
+        this.coordinateNo3 = coordinateNo3;
+
+    }
+
+    public Object getCoordinateNo1() {
+        return this.coordinateNo1;
+    }
+
+    public void setCoordinateNo1(Object coordinateNo1) {
+        this.coordinateNo1 = coordinateNo1;
+    }
+
+    public Object getCoordinateNo2() {
+        return this.coordinateNo2;
+    }
+
+    public void setCoordinateNo2(Object coordinateNo2) {
+        this.coordinateNo2 = coordinateNo2;
+    }
+
+    public Object getCoordinateNo3() {
+        return this.coordinateNo3;
+    }
+
+    public void setCoordinateNo3(Object coordinateNo3) {
+        this.coordinateNo3 = coordinateNo3;
+    }
+
+    @Override
+    public String toString() {
+        return "Coordinate3D{" +
+                "coordinateNo1=" + this.coordinateNo1 +
+                ", coordinateNo2=" + this.coordinateNo2 +
+                ", coordinateNo3=" + this.coordinateNo3 +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) {
+
+            return true;
+
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+
+            return false;
+
+        }
+
+        Coordinate3D that = (Coordinate3D) o;
+
+        return Objects.equals(this.coordinateNo1, that.coordinateNo1) &&
+                Objects.equals(this.coordinateNo2, that.coordinateNo2) &&
+                Objects.equals(this.coordinateNo3, that.coordinateNo3);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.coordinateNo1, this.coordinateNo2, this.coordinateNo3);
+    }
+}
---
+++ src/main/java/org/quist/qubits/geometry/utils/coordinates/SphericalCoordinate.java
@@ -0,0 +1,83 @@
+package org.quist.qubits.geometry.utils.coordinates;
+
+import java.util.Objects;
+
+public class SphericalCoordinate extends Coordinate3D {
+
+    private double radius;
+    private double theta;
+    private double phi;
+
+    public SphericalCoordinate(double radius, double theta, double phi) {
+
+        super(radius, theta, phi);
+
+        this.radius = radius;
+        this.theta = theta;
+        this.phi = phi;
+
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getTheta() {
+        return this.theta;
+    }
+
+    public void setTheta(double theta) {
+        this.theta = theta;
+    }
+
+    public double getPhi() {
+        return this.phi;
+    }
+
+    public void setPhi(double phi) {
+        this.phi = phi;
+    }
+
+    @Override
+    public String toString() {
+        return "SphericalCoordinate{" +
+                "radius=" + this.radius +
+                ", theta=" + this.theta +
+                ", phi=" + this.phi +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        if (!super.equals(o)) {
+            return false;
+        }
+
+        SphericalCoordinate that = (SphericalCoordinate) o;
+
+        return Double.compare(that.radius, this.radius) == 0 &&
+                Double.compare(that.theta, this.theta) == 0 &&
+                Double.compare(that.phi, this.phi) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(super.hashCode(), this.radius, this.theta, this.phi);
+
+    }
+
+}
