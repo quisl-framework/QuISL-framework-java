5b4124da862cce2ce13fb1bde830136a0be3f5b7
Rúben André Barreiro <r.barreiro@campus.fct.unl.pt>
2020-08-01 09:56:48 +0100

    Uniform3 (U3) Matrix added

--- src/main/java/org/quisl/framework/java/units/qubits/math/linearalgebra/matrix/single/uniform/UniformMatrix.java
+++ src/main/java/org/quisl/framework/java/units/qubits/math/linearalgebra/matrix/single/uniform/UniformMatrix.java
@@ -9,29 +9,45 @@ public class UniformMatrix extends Matrix {
 
     }
 
-    public static ComplexNumber[][] getUniform1Matrix(double phiRadians) {
+    public static ComplexNumber[][] getUniform1Matrix(double lambdaRadians) {
 
         return new ComplexNumber[][]
                        { { ComplexNumber.real_one_img_zero,
                            ComplexNumber.real_zero_img_zero },
                          { ComplexNumber.real_zero_img_zero,
-                           new ComplexNumber( Math.cos(phiRadians), Math.sin(phiRadians) ) } };
+                           new ComplexNumber( Math.cos(lambdaRadians), Math.sin(lambdaRadians) ) } };
 
     }
 
-    public static ComplexNumber[][] getUniform2Matrix(double thetaRadians, double phiRadians) {
+    public static ComplexNumber[][] getUniform2Matrix(double phiRadians, double lambdaRadians) {
 
         return new ComplexNumber[][]
-                       { { new ComplexNumber( ( 1/Math.sqrt(2.0) ) , 0.0 ),
-                           new ComplexNumber(
-                                  -Math.cos(phiRadians)/Math.sqrt(2.0) ,
-                                  -Math.sin(phiRadians)/Math.sqrt(2.0) ) },
-                         { new ComplexNumber(
-                                  Math.cos(thetaRadians)/Math.sqrt(2.0) ,
-                                  Math.sin(thetaRadians)/Math.sqrt(2.0) ),
-                           new ComplexNumber(
-                                  Math.cos(thetaRadians + phiRadians)/Math.sqrt(2.0) ,
-                                  Math.sin(thetaRadians + phiRadians)/Math.sqrt(2.0) ) } };
+                      { { new ComplexNumber( ( 1/Math.sqrt(2.0) ) , 0.0 ),
+                          new ComplexNumber(
+                                -Math.cos(lambdaRadians)/Math.sqrt(2.0) ,
+                                -Math.sin(lambdaRadians)/Math.sqrt(2.0) ) },
+                        { new ComplexNumber(
+                                Math.cos(phiRadians)/Math.sqrt(2.0) ,
+                                Math.sin(phiRadians)/Math.sqrt(2.0) ),
+                          new ComplexNumber(
+                                Math.cos(phiRadians + lambdaRadians)/Math.sqrt(2.0) ,
+                                Math.sin(phiRadians + lambdaRadians)/Math.sqrt(2.0) ) } };
+
+    }
+
+    public static ComplexNumber[][] getUniform3Matrix(double thetaRadians, double phiRadians, double lambdaRadians) {
+
+        return new ComplexNumber[][]
+                      { { new ComplexNumber( Math.cos( ( thetaRadians/2.0 ) ) , 0.0 ),
+                          new ComplexNumber(
+                                  ( ( -Math.cos(lambdaRadians)/Math.sqrt(2.0) ) * ( Math.sin( ( thetaRadians / 2.0 ) ) ) ) ,
+                                  ( ( -Math.sin(lambdaRadians)/Math.sqrt(2.0) ) * ( Math.sin( ( thetaRadians / 2.0 ) ) ) ) ) },
+                        { new ComplexNumber(
+                                ( ( Math.cos(phiRadians)/Math.sqrt(2.0) ) * ( Math.sin( ( thetaRadians / 2.0 ) ) ) ) ,
+                                ( ( Math.sin(phiRadians)/Math.sqrt(2.0) ) * ( Math.sin( ( thetaRadians / 2.0 ) ) ) ) ),
+                          new ComplexNumber(
+                                ( ( Math.cos(phiRadians + lambdaRadians)/Math.sqrt(2.0) ) * ( Math.cos( ( thetaRadians / 2.0 ) ) ) ),
+                                ( ( Math.sin(phiRadians + lambdaRadians)/Math.sqrt(2.0) ) * ( Math.cos( ( thetaRadians / 2.0 ) ) ) ) ) } };
 
     }
 
