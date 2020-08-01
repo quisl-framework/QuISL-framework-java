102445a59138821fe2e290047d7d478551715bbb
Rúben André Barreiro <r.barreiro@campus.fct.unl.pt>
2020-08-01 13:22:12 +0100

    Common Matrix Class added

---
+++ src/main/java/org/quisl/framework/java/units/qubits/math/linearalgebra/matrix/common/CommonMatrix.java
@@ -0,0 +1,11 @@
+package org.quisl.framework.java.units.qubits.math.linearalgebra.matrix.common;
+
+import org.quisl.framework.java.units.qubits.math.linearalgebra.Matrix;
+
+public class CommonMatrix extends Matrix {
+
+    public CommonMatrix() {
+
+    }
+
+}
