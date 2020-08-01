47f8e5de76794464a348b28f0bab3aa9d99be220
Rúben André Barreiro <r.barreiro@campus.fct.unl.pt>
2020-08-01 03:36:48 +0100

    Some minor changes added

--- src/main/java/org/quisl/framework/java/units/qubits/math/linearalgebra/matrix/single/uniform/UniformMatrix.java
+++ src/main/java/org/quisl/framework/java/units/qubits/math/linearalgebra/matrix/single/uniform/UniformMatrix.java
@@ -22,16 +22,16 @@ public class UniformMatrix extends Matrix {
     public static ComplexNumber[][] getUniform2Matrix(double thetaRadians, double phiRadians) {
 
         return new ComplexNumber[][]
-                      { { new ComplexNumber( ( 1/Math.sqrt(2.0) ) , 0.0 ),
-                          new ComplexNumber(
+                       { { new ComplexNumber( ( 1/Math.sqrt(2.0) ) , 0.0 ),
+                           new ComplexNumber(
                                   -Math.cos(phiRadians)/Math.sqrt(2.0) ,
                                   -Math.sin(phiRadians)/Math.sqrt(2.0) ) },
-                        { new ComplexNumber(
-                                Math.cos(thetaRadians)/Math.sqrt(2.0) ,
-                                Math.sin(thetaRadians)/Math.sqrt(2.0) ),
-                          new ComplexNumber(
-                                Math.cos(thetaRadians + phiRadians)/Math.sqrt(2.0) ,
-                                Math.sin(thetaRadians + phiRadians)/Math.sqrt(2.0) ) } };
+                         { new ComplexNumber(
+                                  Math.cos(thetaRadians)/Math.sqrt(2.0) ,
+                                  Math.sin(thetaRadians)/Math.sqrt(2.0) ),
+                           new ComplexNumber(
+                                  Math.cos(thetaRadians + phiRadians)/Math.sqrt(2.0) ,
+                                  Math.sin(thetaRadians + phiRadians)/Math.sqrt(2.0) ) } };
 
     }
 
