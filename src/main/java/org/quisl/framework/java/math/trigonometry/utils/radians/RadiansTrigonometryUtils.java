2dfad37c49bb6c0fb35d5eeb893c4a8c60b1c746
Rúben André Barreiro <r.barreiro@campus.fct.unl.pt>
2020-08-02 21:39:48 +0100

    Some Trigonometry (in Radians) Utilities added

---
+++ src/main/java/org/quisl/framework/java/math/trigonometry/utils/radians/RadiansTrigonometryUtils.java
@@ -0,0 +1,101 @@
+package org.quisl.framework.java.math.trigonometry.utils.radians;
+
+import org.quisl.framework.java.math.trigonometry.utils.TrigonometryUtils;
+
+public class RadiansTrigonometryUtils extends TrigonometryUtils {
+
+    public static final double ANGLE_0_RADIANS = 0;
+
+    public static final double ANGLE_PI_DIV_20_RADIANS = ( Math.PI / 20.0 );
+
+    public static final double ANGLE_PI_DIV_19_RADIANS = ( Math.PI / 19.0 );
+
+    public static final double ANGLE_PI_DIV_18_RADIANS = ( Math.PI / 18.0 );
+
+    public static final double ANGLE_PI_DIV_17_RADIANS = ( Math.PI / 17.0 );
+
+    public static final double ANGLE_PI_DIV_16_RADIANS = ( Math.PI / 16.0 );
+
+    public static final double ANGLE_PI_DIV_15_RADIANS = ( Math.PI / 15.0 );
+
+    public static final double ANGLE_PI_DIV_14_RADIANS = ( Math.PI / 14.0 );
+
+    public static final double ANGLE_PI_DIV_13_RADIANS = ( Math.PI / 13.0 );
+
+    public static final double ANGLE_PI_DIV_12_RADIANS = ( Math.PI / 12.0 );
+
+    public static final double ANGLE_PI_DIV_11_RADIANS = ( Math.PI / 11.0 );
+
+    public static final double ANGLE_PI_DIV_10_RADIANS = ( Math.PI / 10.0 );
+
+    public static final double ANGLE_PI_DIV_9_RADIANS = ( Math.PI / 9.0 );
+
+    public static final double ANGLE_PI_DIV_8_RADIANS = ( Math.PI / 8.0 );
+
+    public static final double ANGLE_PI_DIV_7_RADIANS = ( Math.PI / 7.0 );
+
+    public static final double ANGLE_PI_DIV_6_RADIANS = ( Math.PI / 6.0 );
+
+    public static final double ANGLE_PI_DIV_5_RADIANS = ( Math.PI / 5.0 );
+
+    public static final double ANGLE_PI_DIV_4_RADIANS = ( Math.PI / 4.0 );
+
+    public static final double ANGLE_PI_DIV_3_RADIANS = ( Math.PI / 3.0 );
+
+    public static final double ANGLE_PI_DIV_2_RADIANS = ( Math.PI / 2.0 );
+
+    public static final double ANGLE_PI_RADIANS = Math.PI;
+
+    public static final double ANGLE_2_PI_RADIANS = ( 2.0 * Math.PI );
+
+
+
+    public static final double ANGLE_0_RADIANS_TO_DEGREES = TrigonometryUtils.radiansToDegrees(0);
+
+    public static final double ANGLE_PI_DIV_20_RADIANS_TO_DEGREES = TrigonometryUtils.radiansToDegrees( ( Math.PI / 20.0 ) );
+
+    public static final double ANGLE_PI_DIV_19_RADIANS_TO_DEGREES = TrigonometryUtils.radiansToDegrees( ( Math.PI / 19.0 ) );
+
+    public static final double ANGLE_PI_DIV_18_RADIANS_TO_DEGREES = TrigonometryUtils.radiansToDegrees( ( Math.PI / 18.0 ) );
+
+    public static final double ANGLE_PI_DIV_17_RADIANS_TO_DEGREES = TrigonometryUtils.radiansToDegrees( ( Math.PI / 17.0 ) );
+
+    public static final double ANGLE_PI_DIV_16_RADIANS_TO_DEGREES = TrigonometryUtils.radiansToDegrees( ( Math.PI / 16.0 ) );
+
+    public static final double ANGLE_PI_DIV_15_RADIANS_TO_DEGREES = TrigonometryUtils.radiansToDegrees( ( Math.PI / 15.0 ) );
+
+    public static final double ANGLE_PI_DIV_14_RADIANS_TO_DEGREES = TrigonometryUtils.radiansToDegrees( ( Math.PI / 14.0 ) );
+
+    public static final double ANGLE_PI_DIV_13_RADIANS_TO_DEGREES = TrigonometryUtils.radiansToDegrees( ( Math.PI / 13.0 ) );
+
+    public static final double ANGLE_PI_DIV_12_RADIANS_TO_DEGREES = TrigonometryUtils.radiansToDegrees( ( Math.PI / 12.0 ) );
+
+    public static final double ANGLE_PI_DIV_11_RADIANS_TO_DEGREES = TrigonometryUtils.radiansToDegrees( ( Math.PI / 11.0 ) );
+
+    public static final double ANGLE_PI_DIV_10_RADIANS_TO_DEGREES = TrigonometryUtils.radiansToDegrees( ( Math.PI / 10.0 ) );
+
+    public static final double ANGLE_PI_DIV_9_RADIANS_TO_DEGREES = TrigonometryUtils.radiansToDegrees( ( Math.PI / 9.0 ) );
+
+    public static final double ANGLE_PI_DIV_8_RADIANS_TO_DEGREES = TrigonometryUtils.radiansToDegrees( ( Math.PI / 8.0 ) );
+
+    public static final double ANGLE_PI_DIV_7_RADIANS_TO_DEGREES = TrigonometryUtils.radiansToDegrees( ( Math.PI / 7.0 ) );
+
+    public static final double ANGLE_PI_DIV_6_RADIANS_TO_DEGREES = TrigonometryUtils.radiansToDegrees( ( Math.PI / 6.0 ) );
+
+    public static final double ANGLE_PI_DIV_5_RADIANS_TO_DEGREES = TrigonometryUtils.radiansToDegrees( ( Math.PI / 5.0 ) );
+
+    public static final double ANGLE_PI_DIV_4_RADIANS_TO_DEGREES = TrigonometryUtils.radiansToDegrees( ( Math.PI / 4.0 ) );
+
+    public static final double ANGLE_PI_DIV_3_RADIANS_TO_DEGREES = TrigonometryUtils.radiansToDegrees( ( Math.PI / 3.0 ) );
+
+    public static final double ANGLE_PI_DIV_2_RADIANS_TO_DEGREES = TrigonometryUtils.radiansToDegrees( ( Math.PI / 2.0 ) );
+
+    public static final double ANGLE_PI_RADIANS_TO_DEGREES = TrigonometryUtils.radiansToDegrees( Math.PI );
+
+
+
+    public RadiansTrigonometryUtils() {
+
+    }
+
+}
