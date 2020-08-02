5cead5a80b7a48c02d209c739a0ddb89af05917f
Rúben André Barreiro <r.barreiro@campus.fct.unl.pt>
2020-08-02 21:39:57 +0100

    Some Trigonometry (in Degrees) Utilities added

---
+++ src/main/java/org/quisl/framework/java/math/trigonometry/utils/degrees/DegreesTrigonometryUtils.java
@@ -0,0 +1,307 @@
+package org.quisl.framework.java.math.trigonometry.utils.degrees;
+
+import org.quisl.framework.java.math.trigonometry.utils.TrigonometryUtils;
+
+public class DegreesTrigonometryUtils extends TrigonometryUtils {
+
+    public double ANGLE_0_DEGREES = 0;
+
+    public double ANGLE_5_DEGREES = 5;
+
+    public double ANGLE_10_DEGREES = 10;
+
+    public double ANGLE_15_DEGREES = 15;
+
+    public double ANGLE_20_DEGREES = 20;
+
+    public double ANGLE_25_DEGREES = 25;
+
+    public double ANGLE_30_DEGREES = 30;
+
+    public double ANGLE_35_DEGREES = 35;
+
+    public double ANGLE_40_DEGREES = 40;
+
+    public double ANGLE_45_DEGREES = 45;
+
+    public double ANGLE_50_DEGREES = 50;
+
+    public double ANGLE_55_DEGREES = 55;
+
+    public double ANGLE_60_DEGREES = 60;
+
+    public double ANGLE_65_DEGREES = 65;
+
+    public double ANGLE_70_DEGREES = 70;
+
+    public double ANGLE_75_DEGREES = 75;
+
+    public double ANGLE_80_DEGREES = 80;
+
+    public double ANGLE_85_DEGREES = 85;
+
+    public double ANGLE_90_DEGREES = 90;
+
+    public double ANGLE_95_DEGREES = 95;
+
+    public double ANGLE_100_DEGREES = 100;
+
+    public double ANGLE_105_DEGREES = 105;
+
+    public double ANGLE_110_DEGREES = 110;
+
+    public double ANGLE_115_DEGREES = 115;
+
+    public double ANGLE_120_DEGREES = 120;
+
+    public double ANGLE_125_DEGREES = 125;
+
+    public double ANGLE_130_DEGREES = 130;
+
+    public double ANGLE_135_DEGREES = 135;
+
+    public double ANGLE_140_DEGREES = 140;
+
+    public double ANGLE_145_DEGREES = 145;
+
+    public double ANGLE_150_DEGREES = 150;
+
+    public double ANGLE_155_DEGREES = 155;
+
+    public double ANGLE_160_DEGREES = 160;
+
+    public double ANGLE_165_DEGREES = 165;
+
+    public double ANGLE_170_DEGREES = 170;
+
+    public double ANGLE_175_DEGREES = 175;
+
+    public double ANGLE_180_DEGREES = 180;
+
+    public double ANGLE_185_DEGREES = 185;
+
+    public double ANGLE_190_DEGREES = 190;
+
+    public double ANGLE_195_DEGREES = 195;
+
+    public double ANGLE_200_DEGREES = 200;
+
+    public double ANGLE_205_DEGREES = 205;
+
+    public double ANGLE_210_DEGREES = 210;
+
+    public double ANGLE_215_DEGREES = 215;
+
+    public double ANGLE_220_DEGREES = 220;
+
+    public double ANGLE_225_DEGREES = 225;
+
+    public double ANGLE_230_DEGREES = 230;
+
+    public double ANGLE_235_DEGREES = 235;
+
+    public double ANGLE_240_DEGREES = 240;
+
+    public double ANGLE_245_DEGREES = 245;
+
+    public double ANGLE_250_DEGREES = 250;
+
+    public double ANGLE_255_DEGREES = 255;
+
+    public double ANGLE_260_DEGREES = 260;
+
+    public double ANGLE_265_DEGREES = 265;
+
+    public double ANGLE_270_DEGREES = 270;
+
+    public double ANGLE_275_DEGREES = 275;
+
+    public double ANGLE_280_DEGREES = 280;
+
+    public double ANGLE_285_DEGREES = 285;
+
+    public double ANGLE_290_DEGREES = 290;
+
+    public double ANGLE_295_DEGREES = 295;
+
+    public double ANGLE_300_DEGREES = 300;
+
+    public double ANGLE_305_DEGREES = 305;
+
+    public double ANGLE_310_DEGREES = 310;
+
+    public double ANGLE_315_DEGREES = 315;
+
+    public double ANGLE_320_DEGREES = 320;
+
+    public double ANGLE_325_DEGREES = 325;
+
+    public double ANGLE_330_DEGREES = 330;
+
+    public double ANGLE_335_DEGREES = 335;
+
+    public double ANGLE_340_DEGREES = 340;
+
+    public double ANGLE_345_DEGREES = 345;
+
+    public double ANGLE_350_DEGREES = 350;
+
+    public double ANGLE_355_DEGREES = 355;
+
+    public double ANGLE_360_DEGREES = 360;
+
+
+
+    public double ANGLE_0_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(0);
+
+    public double ANGLE_5_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(5);
+
+    public double ANGLE_10_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(10);
+
+    public double ANGLE_15_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(15);
+
+    public double ANGLE_20_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(20);
+
+    public double ANGLE_25_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(25);
+
+    public double ANGLE_30_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(30);
+
+    public double ANGLE_35_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(35);
+
+    public double ANGLE_40_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(40);
+
+    public double ANGLE_45_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(45);
+
+    public double ANGLE_50_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(50);
+
+    public double ANGLE_55_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(55);
+
+    public double ANGLE_60_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(60);
+
+    public double ANGLE_65_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(65);
+
+    public double ANGLE_70_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(70);
+
+    public double ANGLE_75_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(75);
+
+    public double ANGLE_80_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(80);
+
+    public double ANGLE_85_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(85);
+
+    public double ANGLE_90_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(90);
+
+    public double ANGLE_95_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(95);
+
+    public double ANGLE_100_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(100);
+
+    public double ANGLE_105_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(105);
+
+    public double ANGLE_110_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(110);
+
+    public double ANGLE_115_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(115);
+
+    public double ANGLE_120_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(120);
+
+    public double ANGLE_125_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(125);
+
+    public double ANGLE_130_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(130);
+
+    public double ANGLE_135_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(135);
+
+    public double ANGLE_140_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(140);
+
+    public double ANGLE_145_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(145);
+
+    public double ANGLE_150_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(150);
+
+    public double ANGLE_155_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(155);
+
+    public double ANGLE_160_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(160);
+
+    public double ANGLE_165_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(165);
+
+    public double ANGLE_170_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(170);
+
+    public double ANGLE_175_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(175);
+
+    public double ANGLE_180_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(180);
+
+    public double ANGLE_185_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(185);
+
+    public double ANGLE_190_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(190);
+
+    public double ANGLE_195_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(195);
+
+    public double ANGLE_200_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(200);
+
+    public double ANGLE_205_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(205);
+
+    public double ANGLE_210_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(210);
+
+    public double ANGLE_215_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(215);
+
+    public double ANGLE_220_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(220);
+
+    public double ANGLE_225_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(225);
+
+    public double ANGLE_230_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(230);
+
+    public double ANGLE_235_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(235);
+
+    public double ANGLE_240_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(240);
+
+    public double ANGLE_245_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(245);
+
+    public double ANGLE_250_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(250);
+
+    public double ANGLE_255_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(255);
+
+    public double ANGLE_260_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(260);
+
+    public double ANGLE_265_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(265);
+
+    public double ANGLE_270_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(270);
+
+    public double ANGLE_275_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(275);
+
+    public double ANGLE_280_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(280);
+
+    public double ANGLE_285_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(285);
+
+    public double ANGLE_290_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(290);
+
+    public double ANGLE_295_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(295);
+
+    public double ANGLE_300_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(300);
+
+    public double ANGLE_305_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(305);
+
+    public double ANGLE_310_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(310);
+
+    public double ANGLE_315_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(315);
+
+    public double ANGLE_320_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(320);
+
+    public double ANGLE_325_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(325);
+
+    public double ANGLE_330_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(330);
+
+    public double ANGLE_335_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(335);
+
+    public double ANGLE_340_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(340);
+
+    public double ANGLE_345_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(345);
+
+    public double ANGLE_350_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(350);
+
+    public double ANGLE_355_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(355);
+
+    public double ANGLE_360_DEGREES_TO_RADIANS = TrigonometryUtils.degreesToRadians(360);
+
+
+
+    public DegreesTrigonometryUtils() {
+
+    }
+
+}
