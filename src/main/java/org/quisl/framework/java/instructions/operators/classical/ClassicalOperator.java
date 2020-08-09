0aa2883e4e53cf2d9114e2b77c1bb90042507f15
Rúben André Barreiro <r.barreiro@campus.fct.unl.pt>
2020-08-09 10:03:40 +0100

    Classical Operator added

---
+++ src/main/java/org/quisl/framework/java/instructions/operators/classical/ClassicalOperator.java
@@ -0,0 +1,13 @@
+package org.quisl.framework.java.instructions.operators.classical;
+
+import org.quisl.framework.java.instructions.operators.Operator;
+
+public abstract class ClassicalOperator extends Operator {
+
+    public ClassicalOperator(Long operatorId) {
+        super(operatorId);
+    }
+
+
+
+}
