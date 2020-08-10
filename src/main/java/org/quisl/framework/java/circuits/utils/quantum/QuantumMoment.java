8950aff226cb143c517575d55cd8245d273bcd0d
Rúben André Barreiro <r.barreiro@campus.fct.unl.pt>
2020-08-10 11:34:51 +0100

    Some changes in Quantum Moment for Quantum Circuits added

--- src/main/java/org/quisl/framework/java/circuits/utils/quantum/QuantumMoment.java
+++ src/main/java/org/quisl/framework/java/circuits/utils/quantum/QuantumMoment.java
@@ -2,12 +2,28 @@ package org.quisl.framework.java.circuits.utils.quantum;
 
 import org.quisl.framework.java.circuits.utils.Moment;
 import org.quisl.framework.java.circuits.utils.common.MomentPrefixes;
+import org.quisl.framework.java.instructions.operators.quantum.QuantumOperator;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class QuantumMoment extends Moment {
+
+    private Map<Long, QuantumOperator> quantumOperators;
+
 
     public QuantumMoment(Long numMoment) {
 
         super(numMoment, MomentPrefixes.QUANTUM_MOMENT.getMomentPrefix());
+
+        this.quantumOperators = new ConcurrentHashMap<>();
+
+    }
+
+
+    public Map<Long, QuantumOperator> getQuantumOperators() {
+
+        return this.quantumOperators;
 
     }
 
