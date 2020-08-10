1de67785506a65e970787ff83649d64bc6c8061c
Rúben André Barreiro <r.barreiro@campus.fct.unl.pt>
2020-08-10 11:34:03 +0100

    Some changes in Classical Circuit added

--- src/main/java/org/quisl/framework/java/circuits/classical/ClassicalCircuit.java
+++ src/main/java/org/quisl/framework/java/circuits/classical/ClassicalCircuit.java
@@ -1,9 +1,10 @@
 package org.quisl.framework.java.circuits.classical;
 
 import org.quisl.framework.java.circuits.Circuit;
-import org.quisl.framework.java.circuits.common.CircuitPrefixes;
 import org.quisl.framework.java.circuits.utils.classical.ClassicalMoment;
+import org.quisl.framework.java.common.ComputingPrefixes;
 import org.quisl.framework.java.registers.classical.ClassicalRegister;
+import org.quisl.framework.java.units.computing.classical.binary.deterministic.bits.Bit;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -16,8 +17,8 @@ public class ClassicalCircuit extends Circuit {
 
     public ClassicalCircuit(Long id, ClassicalRegister ... classicalRegisters) {
 
-        super( id, CircuitPrefixes.CLASSICAL_CIRCUIT.getCircuitPrefix(),
-             ( "cl-circ-" + id ), classicalRegisters);
+        super( id, ComputingPrefixes.CLASSICAL_PREFIX.getComputingPrefix(),
+             ( "cl-circ-" + id ), classicalRegisters );
 
         this.classicalRegisters = new ConcurrentHashMap<>();
 
@@ -39,8 +40,7 @@ public class ClassicalCircuit extends Circuit {
 
     public ClassicalCircuit(Long id, String classicalCircuitName, ClassicalRegister ... classicalRegisters) {
 
-        super( id, CircuitPrefixes.CLASSICAL_CIRCUIT.getCircuitPrefix(),
-               classicalCircuitName, classicalRegisters);
+        super( id, ComputingPrefixes.CLASSICAL_PREFIX.getComputingPrefix(), classicalCircuitName, classicalRegisters );
 
         this.classicalRegisters = new ConcurrentHashMap<>();
 
@@ -60,5 +60,42 @@ public class ClassicalCircuit extends Circuit {
 
     }
 
+
+    public void NOT(Long bitId) {
+
+        boolean isBitFound = false;
+
+        Bit bitFound = null;
+
+
+        for(ClassicalRegister classicalRegister : this.classicalRegisters.values()) {
+
+            Map<Long, Bit> classicalRegisterBits = classicalRegister.getBits();
+
+            if (classicalRegisterBits.containsKey(bitId)) {
+
+                isBitFound = true;
+
+                bitFound = classicalRegisterBits.get(bitId);
+
+                break;
+
+            }
+
+        }
+
+
+        if( isBitFound ) {
+
+            NOT(bitId);
+
+        }
+        else {
+
+
+
+        }
+
+    }
 
 }
