6ddf21440be4924252e2a96891d6494751dfe40b
Rúben André Barreiro <r.barreiro@campus.fct.unl.pt>
2020-08-10 11:32:52 +0100

    Abstract Classical Operator added

--- src/main/java/org/quisl/framework/java/instructions/operators/classical/ClassicalOperator.java
+++ src/main/java/org/quisl/framework/java/instructions/operators/classical/ClassicalOperator.java
@@ -1,13 +1,46 @@
 package org.quisl.framework.java.instructions.operators.classical;
 
+import org.quisl.framework.java.common.ComputingPrefixes;
+import org.quisl.framework.java.instructions.gates.Gate;
+import org.quisl.framework.java.instructions.gates.classical.ClassicalGate;
 import org.quisl.framework.java.instructions.operators.Operator;
+import org.quisl.framework.java.units.computing.classical.binary.deterministic.bits.Bit;
 
 public abstract class ClassicalOperator extends Operator {
 
-    public ClassicalOperator(Long operatorId) {
-        super(operatorId);
+    private final Integer numBitsInput;
+
+    private final Integer numBitsOutput;
+
+    private ClassicalGate classicalGate;
+
+
+    public ClassicalOperator(Long operatorId, Integer numBitsInput, Integer numBitsOutput) {
+
+        super(operatorId, ComputingPrefixes.CLASSICAL_PREFIX.getComputingPrefix());
+
+        this.numBitsInput = numBitsInput;
+        this.numBitsOutput = numBitsOutput;
+
     }
 
 
+    public Integer getNumBitsInput() {
+
+        return this.numBitsInput;
+
+    }
+
+    public Integer getNumBitsOutput() {
+
+        return this.numBitsOutput;
+
+    }
+
+    public ClassicalGate getClassicalGate() {
+
+        return this.classicalGate;
+
+    }
 
 }
