f9d9323ccb84c7e005893b4b2899a5d5a5c5c96d
Rúben André Barreiro <r.barreiro@campus.fct.unl.pt>
2020-08-10 11:32:39 +0100

    Abstract Operator added

--- src/main/java/org/quisl/framework/java/instructions/operators/Operator.java
+++ src/main/java/org/quisl/framework/java/instructions/operators/Operator.java
@@ -2,18 +2,34 @@ package org.quisl.framework.java.instructions.operators;
 
 public abstract class Operator {
 
-    private Long operatorId;
+    private final Long operatorId;
 
-    public Operator(Long operatorId) {
+    private final String prefixComputing;
+
+
+    public Operator(Long operatorId, String prefixComputing) {
 
         this.operatorId = operatorId;
 
+        this.prefixComputing = prefixComputing;
+
     }
+
 
     public Long getOperatorId() {
 
         return this.operatorId;
 
     }
+
+    public String getPrefixComputing() {
+
+        return this.prefixComputing;
+
+    }
+
+    public abstract void setupGate(Object[] inputs, Object[] outputs, String gateName) throws Exception;
+
+    public abstract void applyGate();
 
 }
